a913e713fc74074eb0ca2fd8b933bc27315d0779
Lindsay Wall <lindsay.is@gmail.com>
2017-09-04 19:14:45 -0400

    solved all of the last challenges, psychic game is a win

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,34 @@
 var possGuesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-	"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var wins = 0
-var losses = 0
-var guessesLeft = 9
+	"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
 var compGuess = possGuesses[Math.floor(Math.random() * 26 + 1)]
 console.log(compGuess);
 var yourGuesses = [];
 
-// What if I put the if/else statement outside of this function, so if gl > 0, run this, else loss++
+// Determins how the event key is processed.
 document.onkeyup = function(event) {
     var yourChoice = event.key;
     console.log(yourChoice); //works to log the key pressed to console! 
 
+    // Alerts if the choice isn't a valid lette.r
+    if (possGuesses.includes(yourChoice) === false) {
+    	alert("That's not a letter, silly!");
+    	guessesLeft++; 
+    }
+
+    // Pushes the key choice to the guess array to show on screen.
     guessArray(yourChoice);
 
+    // If guesses left aren't 0, compares the user choice. Still don't know how to new choice.
     if (guessesLeft >= 1) {
     	var compChoice = compGuess;
     	console.log(compChoice);
     	compare(yourChoice, compChoice);
     }
-
-    else {
-    	getCompGuess();
-    	console.log("I get here");
-    }
    	
+   	// Displays the results of key choices, guess array, and wins/losses.
    	render();
 };
 
@@ -32,7 +36,9 @@ document.onkeyup = function(event) {
 function compare(val1, val2) {
 	if (val1 === val2) {
       	wins++; 
-      	guessesLeft = 9;
+      	console.log("round increased bc win");
+      	guessesLeft = 9; 
+      	compGuess = possGuesses[Math.floor(Math.random() * 26 + 1)]; // resets comp guess
       	yourGuesses.splice(0,10);
       	console.log("Match!");
       	console.log(wins);
@@ -46,12 +52,15 @@ function compare(val1, val2) {
       }
       	else {
       		losses++;
+      		console.log("round increased bc loss");
       		guessesLeft = 9;
+      		compGuess = possGuesses[Math.floor(Math.random() * 26 + 1)]; // resets comp guess
       		yourGuesses.splice(0,10);
       	}
     }
 }
 
+// Function to write choices to the document.
 function render() {
 	document.getElementById("wins").innerText = wins;
 	document.getElementById("loss").innerText = losses;
@@ -59,27 +68,14 @@ function render() {
 	document.getElementById("humanGuess").innerText = yourGuesses;
 }
 
+// Determines if the value has already been guessed, if not then pushes to the guessarray.
 function guessArray(val1) {
-	yourGuesses.push(val1);
+	if (yourGuesses.includes(val1) === true) {
+		alert("You've already guessed that one!");
+		guessesLeft++;
+	}
+
+	else if (possGuesses.includes(val1) === true) {
+		yourGuesses.push(val1);
+	}
 }
-
-// function getCompGuess() {
-// 	value = possGuesses[Math.floor(Math.random() * 26 + 1)];
-// 	compGuess.push(value);
-// }
-
-//this works to reduce guessesLeft from 9 to 0! woo
-// for (var i = guessesLeft; i > 0; i--) { 
-  	// if (possGuesses === compGuess) {
-   //    	wins++;
-   //    	console.log("oops");
-   //  }
-   //  else {
-   //    	guessesLeft--;
-   //    	console.log("I get here");
-   //    	console.log(guessesLeft);
-   //    	}
-    // }
-      
-// Need a way/function to write the results to the html file.
-// Need a way to alert if choice is invalid. if yourChoice = (possGuesses.indexOf[yourChoice] < 0), alert?
